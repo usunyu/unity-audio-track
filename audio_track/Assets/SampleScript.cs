e7f3cbbb651583c2ec3060e123de6db6a45974d7
Yu Sun <usunyu@gmail.com>
2017-11-07 16:28:57 +0800

    Add ui output for testing on build

--- audio_track/Assets/SampleScript.cs
+++ audio_track/Assets/SampleScript.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Audio;
 
 public class SampleScript : MonoBehaviour
 {
+    public Text[] texts;
+    private AudioTrackInfo[] _tracks = null;
+
     // Use this for initialization
     void Start()
     {
         string mediaFile = Application.dataPath + "/../../Track.mp4";
-        AudioTrackInfo[] tracks = AudioTrack.Instance.QueryAudioTrack(mediaFile);
-        foreach (AudioTrackInfo track in tracks)
+        //string mediaFile = "c:/Users/usuny/Desktop/Track.mp4";
+        _tracks = AudioTrack.Instance.QueryAudioTrack(mediaFile);
+        int i = 0;
+        foreach (AudioTrackInfo track in _tracks)
         {
             Debug.Log(track.ToString());
+            if (i < texts.Length)
+                texts[i].text = track.ToString();
+            i++;
         }
     }
 
